2cf97679fc314bbf9c2dfa019d4529c4cd312111
kilianpr <court.killer1.0@gmail.com>
2021-09-07 17:32:10 +0200

    Implemented old JavaScript scripts and added new function to detect when page was loaded from history and if that's the case set opacity to 1

--- scripts/home.js
+++ scripts/home.js
@@ -1,3 +1,12 @@
+//checks if page was loaded from history, if yes, it reloads since otherwise opacity=0
+window.addEventListener( "pageshow", function ( event ) {
+    if ( event.persisted ) {
+      window.location.reload();
+    }
+  });
+
+
+//pulls up hamburger menu if user clicks outside the menu
 window.addEventListener('click', function(e){
     if (!document.getElementById("menuToggle").contains(e.target)){
         if (document.getElementById("checkbox").checked){
@@ -6,6 +15,15 @@ window.addEventListener('click', function(e){
     }
 });
 
+// sets opacity to 0 when going to different page (which triggers css animation)
+window.transitionToPage = function(href){
+    document.querySelector('body').style.opacity = 0;
+    setTimeout(function(){
+        window.location.href = href;
+    }, 1000);
+}
+
+//adds remove-hashes functionality to buttons and sets opacity of body to 1
 document.addEventListener("DOMContentLoaded", function(){
     const menuBtns = document.getElementsByClassName('remove-hash');
     for (var i=0; i<menuBtns.length; i++){
@@ -21,9 +39,20 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         }
     }
-    document.querySelector('body').style.opacity = 1
+    document.querySelector('body').style.opacity = 1;
 });
 
 function removeHash(){
     history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
+}
+
+
+function openClient(){
+    const name = document.getElementById("name").value;
+    const subject = document.getElementById("subject").value;
+    const mail_content = document.getElementById("mail-content").value;
+    const body = mail_content + '\n' + '\n' + 'Best Regards' + '\n' + name;
+    console.log(mail_content);
+    console.log(body);
+    location.href = 'mailto:leander.machan@gmail.com?subject='+subject+'&body='+encodeURIComponent(body);
 }
