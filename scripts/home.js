ab04d30eb5f595e4820e0c06462475abcf09ef4b
kilianpr <court.killer1.0@gmail.com>
2021-09-07 16:19:22 +0200

    Added padding-top to each #content h1 so the heading fits in view when auto scrolling with anchor tag; increased space between content sections

--- scripts/home.js
+++ scripts/home.js
@@ -6,3 +6,24 @@ window.addEventListener('click', function(e){
     }
 });
 
+document.addEventListener("DOMContentLoaded", function(){
+    const menuBtns = document.getElementsByClassName('remove-hash');
+    for (var i=0; i<menuBtns.length; i++){
+        var menuBtn = menuBtns[i];
+        menuBtn.onclick = function(){
+            setTimeout(()=>{
+                removeHash();
+            }, 5);
+            if (document.getElementById("checkbox").checked){
+                setTimeout(()=>{
+                    document.getElementById("checkbox").checked=false;
+                }, 500);   
+            }
+        }
+    }
+    document.querySelector('body').style.opacity = 1
+});
+
+function removeHash(){
+    history.replaceState('', document.title, window.location.origin + window.location.pathname + window.location.search);
+}
